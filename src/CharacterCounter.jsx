46be60caf011d97854694cd09a6c2eeea949a759
BalaPriyaMS <balapriya@intellicar.in>
2024-06-27 11:39:07 +0530

    set state for text

--- src/CharacterCounter.jsx
+++ src/CharacterCounter.jsx
@@ -1,15 +1,20 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 
 const CharacterCounter = () => {
-  const text = "Hi Priya, What are you doing?";
+  const [text, setText] = useState("");
 
+  const textHandle = (e) => {
+    setText(e.target.value);
+  };
   return (
     <div className="character-counter">
       <textarea
-        value={text}
+        // value={text}
         placeholder="Type your text here..."
         rows="5"
         cols="40"
+        onChange={textHandle}
       />
       <p>Character count: {text.length}</p>
     </div>
